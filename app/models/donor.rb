428a2353be9baef8cf86a108d1d4cb547e89ad9c
Puru Dahal <pdahal@me.com>
2015-05-03 02:17:31 -0700

    Normalize and validate data

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -21,10 +21,31 @@
 
 class Donor < ActiveRecord::Base
   geocoded_by :address, :latitude  => :lat, :longitude => :long
+  before_validation :normalize_data
   after_validation :geocode
   has_one :blood_type
 
+  validates :first_name, presence: { :message => 'First name cannot be blank!' }
+  validates :cell_phone, presence: { :message => 'We need cell phone number so we can notify you!' }
+  validates :city, presence: { :message => 'Please tell us which city you live in.' }
+  validates :state, presence: { :message => 'We need your Aanchal so we can find the correct location.' }
+  validates :cell_phone, inclusion: { :in => 10..14, :message => 'Cell phone number you entered in invalid!' }
+
   def address
     [street, city, state, country].compact.join(', ')
+  end
+
+  def fullname
+    [first_name, last_name].compact.join(', ')
+  end
+
+  def normalize_data
+    self.first_name.try(:capitalize!)
+    self.last_name.try(:capitalize!) if self.last_name.present?
+    self.cell_phone.gsub!(/\s|\.|\-|\(|\)/, '').insert(0, '+977') if self.cell_phone.present?
+    self.email.squish.try(:downcase!) if self.contact_person_email.present?
+    self.street.squish.try(:capitalize!) if self.street.present?
+    self.city.squish.try(:capitalize!) if self.city.present?
+    self.state.squish.try(:capitalize!) if self.state.present?
   end
 end
--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -20,10 +20,27 @@
 
 class Hospital < ActiveRecord::Base
   geocoded_by :address, :latitude  => :lat, :longitude => :long
+  before_validation :normalize_data
   after_validation :geocode
   has_and_belongs_to_many :blood_types
 
+  validates :name, presence: { :message => 'Name of the Hospital cannot be blank!' }
+  validates :city, presence: { :message => 'We need city to locate this hospital.' }
+  validates :state, presence: { :message => 'We need Aanchal so we can find the correct location.' }
+  validates :foo, length: {is: 10, message: 'Phone number you entered in invalid!'}, allow_blank: false
+
+
   def address
     [street, city, state, country].compact.join(', ')
+  end
+
+  def normalize_data
+    self.name.try(:capitalize!)
+    self.street.squish.try(:capitalize!) if self.street.present?
+    self.city.squish.try(:capitalize!)
+    self.state.squish.try(:capitalize!)
+    self.contact_person_name.squish.try(:capitalize!) if self.contact_person_name.present?
+    self.contact_person_email.squish.try(:downcase!) if self.contact_person_email.present?
+    self.cell_phone.gsub!(/\s|\.|\-|\(|\)/, '').insert(0, '+977') if self.cell_phone.present?
   end
 end
